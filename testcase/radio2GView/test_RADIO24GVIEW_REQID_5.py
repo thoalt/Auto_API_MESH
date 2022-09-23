68c31d53e0dc94550afd06df61b860440f45db35
Vietha-wireless <114207062+Vietha-wireless@users.noreply.github.com>
2022-09-23 13:49:15 +0700

    Add files via upload

---
+++ testcase/radio2GView/test_RADIO24GVIEW_REQID_5.py
@@ -0,0 +1,25 @@
+import time
+import pytest
+from APIObject.radio24GView import radio24GViewClient
+
+@pytest.mark.usefixtures("login")
+class Test_radio24GView():
+    @pytest.fixture(autouse=True, scope="function")
+    def set_up(self):
+        self.timeOut = 2
+        self.exp = {"code":11 , "msg": "Verify Fail"}
+        self.data = [2147483648]
+        self.radio24GViewClt = radio24GViewClient()
+
+    @pytest.mark.success
+    def test_RADIO24GVIEW_RES_5(self):
+        time.sleep(self.timeOut)
+        resBody_Lst = []
+        for item in self.data:
+          pload = self.radio24GViewClt.Create_radio24GView_Pload(reqID=item)
+          resBody = self.radio24GViewClt.radio24GView(self.cookie, pload=pload).body
+          resBody_Lst.append(resBody)
+        self.radio24GViewClt.assert_response_list(resBody_Lst,
+                                        self.exp['code'],
+                                        self.exp['msg'])
+
