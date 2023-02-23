9f89812b705c5b745e2d8befc313fa9903564def
thoalt <thoalt@vnpt-technology.vn>
2023-02-23 11:06:41 +0700

    ThoaLT: Update Config File for EW30SX

--- Scripts/Login_Then_Create_Cookies.py
+++ Scripts/Login_Then_Create_Cookies.py
@@ -7,14 +7,14 @@ from APIObject.logout import LogoutClient
 from Utilities import Utility as utl
 from Config import config as cfg
 
-# SSH to Mesh AP and restart vnptt_mad agent
-client = SSHClient(hostname=cfg.IP_ADDR_CAP, username=cfg.USER_SSH, password=cfg.PASS_SSH)
-SSHShell = client.create_shell()
-SSHShell.execute_command('/etc/init.d/vnptt_mad stop')
-time.sleep(1)
-SSHShell.execute_command('/etc/init.d/vnptt_mad start')
-time.sleep(1)
-SSHShell.close()
+# # SSH to Mesh AP and restart vnptt_mad agent
+# client = SSHClient(hostname=cfg.IP_ADDR_CAP, username=cfg.USER_SSH, password=cfg.PASS_SSH)
+# SSHShell = client.create_shell()
+# SSHShell.execute_command('/etc/init.d/vnptt_mad stop')
+# time.sleep(1)
+# SSHShell.execute_command('/etc/init.d/vnptt_mad start')
+# time.sleep(1)
+# SSHShell.close()
 
 # Open session then Login
 ClientSes = openssesionClient()
